ba36dcca413cd88ee33df23dbcb14ea2ab9c8ad3
asheezah <aishazahid5@gmail.com>
2026-03-31 14:03:30 +0000

    Fixed About Us. Feel like we could write more, though, I just don't know what to write

--- pages/About Us.py
+++ pages/About Us.py
@@ -1,15 +1,13 @@
 import streamlit as st
 from PIL import Image
 
-img = Image.open("pages/AboutUs.jpeg")
-rotate = img.rotate(180)
-rotate.save("pages/rotate.png")
-
 st.title("Meet the Team")
-#sep = st.columns([3,2.7])
-st.markdown("""It's a :fire: team.
-                     We're in first year iBioMed @ Mac, and Maccesisble is our DP4 project that we built for Jany, a community member with Multiple Sclerosis, so that she could navigate buildings easier.
-                     Ummm I don't know what else to say... Pierce is the GOAT.""")
-st.image('pages/rotate.png', caption = "From right to left: Noah, Erik, Aisha, and Emily.")
+st.divider()
+sep = st.columns([3,2.7])
+sep[0].markdown("""**It's a :fire: team.**  
+                     We're in first year iBioMed @ Mac, and Maccessible is our DP4 project that we built for Jany, 
+                     a community member with Multiple Sclerosis, so that she could navigate buildings easier.    
+                     Pierce is the GOAT.  
+                     **If you want to contact us, go check out Dr McDonald's contact info.**""")
+sep[1].image('pages/rotate.png', caption = "From left to right: Emily, Aisha, Erik, and Noah.")
 
-st.markdown("**If you want to contact us, go check out Dr McDonald's contact info.**")
