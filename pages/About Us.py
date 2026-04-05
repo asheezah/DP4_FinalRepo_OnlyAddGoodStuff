cd4ed581bce5be102e405f582ee7ab41565f7211
asheezah <aishazahid5@gmail.com>
2026-04-05 01:44:09 +0000

    Updated About Us

--- pages/About Us.py
+++ pages/About Us.py
@@ -12,6 +12,10 @@ def about_us():
                         We're in first year iBioMed @ Mac, and Maccessible is our DP4 project that we built for Jany, 
                         a community member with Multiple Sclerosis, so that she could navigate buildings easier.    
                         Pierce is the GOAT.  
+                        Erik: going into Level II iBioMed, specialization: not software (probably), fun fact: He has a banjo.  
+                        Noah: going into Level II iBioMed, specialization: not software, fun fact: He doesn't want to do software.  
+                        Emily: going into Level II iBioMed, specialization: not software, fun fact: She likes green trees.  
+                        Aisha: going into Level II iBioMed, specialization: not software, fun fact: She also likes green trees, but she's allergic to them.  
                         **If you want to contact us, go check out Dr McDonald's contact info.**""")
     sep[1].image('pages/rotate.png', caption = "From left to right: Emily, Aisha, Erik, and Noah.")
 
