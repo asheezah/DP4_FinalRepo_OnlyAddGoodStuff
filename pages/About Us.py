d7509ff0e5fc98cbc84fe014978140090932d903
asheezah <aishazahid5@gmail.com>
2026-04-05 23:29:41 +0000

    Fixed about us again

--- pages/About Us.py
+++ pages/About Us.py
@@ -7,16 +7,8 @@ def about_us():
     ##Create descriptions of the team
     st.title("Meet the Team")
     st.divider()
-    st.image('pages/rotate.png', caption = "From left to right: Emily, Aisha, Erik, and Noah.")
-    st.markdown("""**It's a :fire: team.**  
-                        We're in first year iBioMed @ Mac, and Maccessible is our DP4 project that we built for Jany, 
-                        a community member with Multiple Sclerosis, so that she could navigate buildings easier.    
-                        Pierce is the GOAT.  
-                        Erik: going into Level II iBioMed, specialization: not software (probably), fun fact: He has a banjo.  
-                        Noah: going into Level II iBioMed, specialization: not software, fun fact: He doesn't want to do software.  
-                        Emily: going into Level II iBioMed, specialization: not software, fun fact: She likes green trees.  
-                        Aisha: going into Level II iBioMed, specialization: not software, fun fact: She also likes green trees, but she's allergic to them.  
-                        **If you want to contact us, go check out Dr McDonald's contact info.**""")
+    st.image('pages/Meet the Team.png')
+
 sidebar()
 help_button()
 ##Displays the about us section previously defined.
